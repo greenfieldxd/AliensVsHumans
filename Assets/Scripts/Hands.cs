727f9c4415347265b8b426d1193e09ddeaf85838
greenfieldxd <monsterbig53@gmail.com>
2020-04-03 18:55:45 +0300

    Initial commit

---
+++ Assets/Scripts/Hands.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hands : MonoBehaviour
+{
+    public float minX; // Максимальное и минимальное X координат платформы
+    public float maxX;
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;// позиция мыши в координатах камеры
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos); // позиция мыши в координатах мира
+        //Debug.Log("mousePos: " + mousePos + ", mouseWorldPos: " + mouseWorldPos);
+
+        float xHands = mouseWorldPos.x;// значение x координат мыши
+        float clampedHandsX = Mathf.Clamp(xHands, minX, maxX); // обрезаем координаты платформы по x до нужных нам значений
+        float yHands = transform.position.y;
+
+        transform.position = new Vector3(clampedHandsX, yHands, 0);
+    }
+}
